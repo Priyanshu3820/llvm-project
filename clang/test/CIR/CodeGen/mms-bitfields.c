24ebe74d4d8c73eaf8d1035c3c4589131e1ffe30
Priyanshu3820 <10b.priyanshu@gmail.com>
2025-12-12 23:59:42 +0530

    Fix -mms-bitfields error

--- clang/test/CIR/CodeGen/mms-bitfields.c
+++ clang/test/CIR/CodeGen/mms-bitfields.c
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -mms-bitfields -fclangir -emit-cir %s -o %t.cir
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o %t.cir
 // RUN: FileCheck --input-file=%t.cir %s --check-prefix=CIR
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -mms-bitfields -fclangir -emit-llvm %s -o %t-cir.ll
 // RUN: FileCheck --input-file=%t-cir.ll %s --check-prefix=LLVM
